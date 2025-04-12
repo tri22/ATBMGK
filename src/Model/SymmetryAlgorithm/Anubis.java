db5582ffc6613a6609e487f37142400fdbaea745
Tri Pham <fuinyss1@gmail.com>
2025-04-12 11:35:14 +0700

    add algo

---
+++ src/Model/SymmetryAlgorithm/Anubis.java
@@ -0,0 +1,112 @@
+package Model.SymmetryAlgorithm;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.CipherInputStream;
+import javax.crypto.CipherOutputStream;
+import java.util.Base64;
+
+public class Anubis implements SymmetryAlgorithm {
+
+    private SecretKey secretKey;
+    private Cipher cipher;
+
+    @Override
+    public SecretKey genkey() throws NoSuchAlgorithmException {
+        KeyGenerator keyGenerator = KeyGenerator.getInstance("Anubis");
+        keyGenerator.init(128);  // Anubis thường sử dụng kích thước khóa 128 bit
+        this.secretKey = keyGenerator.generateKey();
+        return this.secretKey;
+    }
+
+    @Override
+    public SecretKey genkey(int keySize) throws NoSuchAlgorithmException {
+        if (keySize != 128 && keySize != 192 && keySize != 256) {
+            throw new IllegalArgumentException("Invalid key size. Valid sizes are 128, 192, and 256 bits.");
+        }
+        KeyGenerator keyGenerator = KeyGenerator.getInstance("Anubis");
+        keyGenerator.init(keySize);  // Người dùng có thể chỉ định kích thước khóa
+        this.secretKey = keyGenerator.generateKey();
+        return this.secretKey;
+    }
+
+    @Override
+    public void loadKey(SecretKey key) {
+        this.secretKey = key;
+    }
+
+    
+    public byte[] encrypt(String text) throws InvalidKeyException, NoSuchAlgorithmException,
+            NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, Exception {
+        cipher = Cipher.getInstance("Anubis");
+        cipher.init(Cipher.ENCRYPT_MODE, this.secretKey);
+        byte[] encrypted = cipher.doFinal(text.getBytes());
+        return encrypted;
+    }
+    
+    @Override
+ 	public String encryptBase64(String text) throws InvalidKeyException, NoSuchAlgorithmException,
+ 			NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, Exception {
+ 		return Base64.getEncoder().encodeToString(encrypt(text));
+ 	}
+
+ 	public String decrypt(String data) throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException,
+ 			IllegalBlockSizeException, BadPaddingException, Exception {
+ 		return decryptBase64(data.getBytes());
+ 	}
+
+    @Override
+    public String decryptBase64(byte[] data) throws InvalidKeyException, NoSuchAlgorithmException,
+            NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, Exception {
+        cipher = Cipher.getInstance("Anubis");
+        cipher.init(Cipher.DECRYPT_MODE, this.secretKey);
+        byte[] decrypted = cipher.doFinal(Base64.getDecoder().decode(data));
+        return new String(decrypted);
+    }
+
+    @Override
+    public boolean encryptFile(String srcf, String desf) throws NoSuchAlgorithmException, NoSuchPaddingException,
+            InvalidKeyException, IOException, IllegalBlockSizeException, BadPaddingException, Exception {
+        FileInputStream fis = new FileInputStream(srcf);
+        FileOutputStream fos = new FileOutputStream(desf);
+        cipher = Cipher.getInstance("Anubis");
+        cipher.init(Cipher.ENCRYPT_MODE, this.secretKey);
+        CipherInputStream cis = new CipherInputStream(fis, cipher);
+        byte[] buffer = new byte[1024];
+        int bytesRead;
+        while ((bytesRead = cis.read(buffer)) != -1) {
+            fos.write(buffer, 0, bytesRead);
+        }
+        cis.close();
+        fos.close();
+        return true;
+    }
+
+    @Override
+    public boolean decryptFile(String srcf, String desf) throws InvalidKeyException, NoSuchAlgorithmException,
+            NoSuchPaddingException, IOException, IllegalBlockSizeException, BadPaddingException, Exception {
+        FileInputStream fis = new FileInputStream(srcf);
+        FileOutputStream fos = new FileOutputStream(desf);
+        cipher = Cipher.getInstance("Anubis");
+        cipher.init(Cipher.DECRYPT_MODE, this.secretKey);
+        CipherOutputStream cos = new CipherOutputStream(fos, cipher);
+        byte[] buffer = new byte[1024];
+        int bytesRead;
+        while ((bytesRead = fis.read(buffer)) != -1) {
+            cos.write(buffer, 0, bytesRead);
+        }
+        cos.close();
+        fis.close();
+        fos.close();
+        return true;
+    }
+}
---
+++ src/Model/SymmetryAlgorithm/ThreeDES.java
@@ -0,0 +1,98 @@
+package Model.SymmetryAlgorithm;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.*;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+
+public class ThreeDES implements SymmetryAlgorithm {
+    private SecretKey secretKey;
+
+    @Override
+    public SecretKey genkey() throws NoSuchAlgorithmException {
+        KeyGenerator keyGen = KeyGenerator.getInstance("DESede");
+        keyGen.init(168); // 3DES dùng 168-bit key
+        secretKey = keyGen.generateKey();
+        return secretKey;
+    }
+
+    @Override
+    public SecretKey genkey(int keySize) throws NoSuchAlgorithmException {
+        KeyGenerator keyGen = KeyGenerator.getInstance("DESede");
+        keyGen.init(keySize);
+        secretKey = keyGen.generateKey();
+        return secretKey;
+    }
+
+    @Override
+    public void loadKey(SecretKey key) {
+        this.secretKey = key;
+    }
+
+    
+    public byte[] encrypt(String text) throws InvalidKeyException, NoSuchAlgorithmException,
+            NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, Exception {
+        Cipher cipher = Cipher.getInstance("DESede");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        byte[] encrypted = cipher.doFinal(text.getBytes());
+        return encrypted;
+    }
+    
+    @Override
+	public String encryptBase64(String text) throws InvalidKeyException, NoSuchAlgorithmException,
+			NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, Exception {
+		return Base64.getEncoder().encodeToString(encrypt(text));
+	}
+
+	public String decrypt(String data) throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException,
+			IllegalBlockSizeException, BadPaddingException, Exception {
+		return decryptBase64(data.getBytes());
+	}
+
+    @Override
+    public String decryptBase64(byte[] data) throws InvalidKeyException, NoSuchAlgorithmException,
+            NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, Exception {
+        Cipher cipher = Cipher.getInstance("DESede");
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+        byte[] decrypted = cipher.doFinal(data);
+        return new String(decrypted);
+    }
+
+    @Override
+    public boolean encryptFile(String srcf, String desf) throws NoSuchAlgorithmException, NoSuchPaddingException,
+            InvalidKeyException, IOException, IllegalBlockSizeException, BadPaddingException, Exception {
+        Cipher cipher = Cipher.getInstance("DESede");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+
+        try (FileInputStream fis = new FileInputStream(srcf);
+             FileOutputStream fos = new FileOutputStream(desf)) {
+            byte[] inputBytes = fis.readAllBytes();
+            byte[] outputBytes = cipher.doFinal(inputBytes);
+            fos.write(outputBytes);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean decryptFile(String srcf, String desf) throws InvalidKeyException, NoSuchAlgorithmException,
+            NoSuchPaddingException, IOException, IllegalBlockSizeException, BadPaddingException, Exception {
+        Cipher cipher = Cipher.getInstance("DESede");
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+
+        try (FileInputStream fis = new FileInputStream(srcf);
+             FileOutputStream fos = new FileOutputStream(desf)) {
+            byte[] inputBytes = fis.readAllBytes();
+            byte[] outputBytes = cipher.doFinal(inputBytes);
+            fos.write(outputBytes);
+        }
+        return true;
+    }
+}
